27c76761c804991a0115cf645e967137a6cdbbf4
philaung96 <philaung96@gmail.com>
2022-01-17 19:56:39 -0800

    projects section update: each project is divided into project and section, project descriptions added and links to project demos

--- src/Components/Body/Home/Home.jsx
+++ src/Components/Body/Home/Home.jsx
@@ -3,21 +3,18 @@ import { Link } from 'react-router-dom';
 import Background from './Background/Background';
 
 const Home = () => {
-    
     return (
         <>
             <Background />
-            <main>
-                <section id='home-info'>
-                    <h1>Phil Aung</h1>
-                    <div id='ufo'></div>
-                    <p>Full Stack Developer</p>
-                    <Link to='/projects'>
-                        <button id='projects-link'>
-                            <p>projects</p>
-                        </button>
-                    </Link>
-                </section>
+            <main id='home-info'>
+                <h1>Phil Aung</h1>
+                <div id='ufo'></div>
+                <p>Full Stack Developer</p>
+                <Link to='/projects'>
+                    <button id='projects-link'>
+                        <p>projects</p>
+                    </button>
+                </Link>
             </main>
         </>
     );
--- src/Components/Body/Projects/Projects.jsx
+++ src/Components/Body/Projects/Projects.jsx
@@ -3,22 +3,97 @@ import './Projects.css';
 const Projects = () => {
     return (
         <div id='projects'>
-            <h2>projects</h2>
-            <section className='project'>
-                <h3 className='project-title'>2048</h3>
-                <div id='project-img-1' className='project-img'></div>
+            <h2>PROJECTS</h2>
+            <div className='project'>
+                <section>
+                    <h3 className='project-title'>2048</h3>
+                    <div id='project-img-1' className='project-img'></div>
+                    <button>
+                        <a
+                            href='https://philaung96.github.io/2048'
+                            target={'_blank'}
+                            rel='noreferrer'
+                        >
+                            {'>'} DEMO
+                        </a>
+                    </button>
+                </section>
                 <article>
-                    <p>A clone 2048 game</p>
+                    <p>
+                        A clone a popular IOS game 2048 made full stack version.
+                        Features include the game itself, localStorage, and
+                        backend database as leaderboard user high scores.
+                    </p>
+                    <p className='tools'>
+                        <small>
+                            Tools :
+                            <i>
+                                {' '}
+                                HTML, CSS, JavaScript, React.js, Express.js,
+                                Mongoose.js, MongoDB
+                            </i>
+                        </small>
+                    </p>
                 </article>
-            </section>
-            <section className='project'>
-                <h3 className='project-title'>Rick and Morty</h3>
-                <div id='project-img-2' className='project-img'></div>
-            </section>
-            <section className='project'>
-                <h3 className='project-title'>To Do List</h3>
-                <div id='project-img-3' className='project-img'></div>
-            </section>
+            </div>
+            <div className='project'>
+                <section>
+                    <h3 className='project-title'>Rick and Morty</h3>
+                    <div id='project-img-2' className='project-img'></div>
+                    <button>
+                        <a
+                            href='https://philaung96.github.io/rick-and-morty-universe'
+                            target={'_blank'}
+                            rel='noreferrer'
+                        >
+                            {'>'} DEMO
+                        </a>
+                    </button>
+                </section>
+                <article>
+                    <p>
+                        Explore the popular animated TV series 'Rick And Morty'.
+                        A front-end web to explore specific character, location,
+                        or aired episode. Created using Rick and Morty API.
+                    </p>
+                    <p className='tools'>
+                        <small>
+                            Tools :<i> HTML, CSS, JavaScript, React.js</i>
+                        </small>
+                    </p>
+                </article>
+            </div>
+            <div className='project'>
+                <section>
+                    <h3 className='project-title'>To Do List</h3>
+                    <div id='project-img-3' className='project-img'></div>
+                    <button>
+                        <a
+                            href='https://philaung96.github.io/jquery-todo-list'
+                            target={'_blank'}
+                            rel='noreferrer'
+                        >
+                            {'>'} DEMO
+                        </a>
+                    </button>
+                </section>
+                <article>
+                    <p>
+                        A simple front-end todo list application. Put all your
+                        daily check marks here, and get the upmost satisfaction
+                        of completing tasks.
+                    </p>
+                    <p className='tools'>
+                        <small>
+                            Tools :
+                            <i>
+                                {' '}
+                                HTML, CSS, JavaScript, Jquery.js
+                            </i>
+                        </small>
+                    </p>
+                </article>
+            </div>
         </div>
     );
 };
