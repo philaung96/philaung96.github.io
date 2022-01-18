775bd61e3c811f700d3dec1efc66a9b9f10a21e3
philaung96 <philaung96@gmail.com>
2022-01-18 01:33:28 -0800

    projects button box shadow change, about page updated: Tech Stack and contact info added with links

--- src/Components/Body/About/About.jsx
+++ src/Components/Body/About/About.jsx
@@ -2,22 +2,62 @@ import './About.css';
 
 const About = () => {
     return (
-        <section id='about'>
-            <div id='avatar'></div>
-            <article>
-                <h2>about</h2>
-                <p>
-                    Hello, my name is Phil Aung. I am a curious and fast
-                    learning Burmese soul from LA. A software engineer with a
-                    strong backend background from college. Well versed on
-                    top-down and bottom-up designing aspects of programs. I am a
-                    team player and aspire to be the best version of me on every
-                    occasion. Born into and raised by Physicists parents, my
-                    love toward electronics runs deep, and I am passionate about
-                    computers with problem solving at the core of it.
-                </p>
-            </article>
-        </section>
+        <div id='about'>
+            <section>
+                <div id='avatar'></div>
+                <article>
+                    <h2>ABOUT</h2>
+                    <p>
+                        Hello, my name is Phil Aung. I am a curious and fast
+                        learning Burmese soul from LA. A software engineer with
+                        a strong backend background from college. Well versed on
+                        top-down and bottom-up designing aspects of programs. I
+                        am a team player and aspire to be the best version of me
+                        on every occasion. Born into and raised by Physicists
+                        parents, my love toward electronics runs deep, and I am
+                        passionate about computers with problem solving at the
+                        core of it.
+                    </p>
+                </article>
+            </section>
+            <aside>
+                <article>
+                    <h3>Tech Stack</h3>
+                    <p>Languages : C++, Java, Python, HTML, CSS, JavaScript</p>
+                    <p>
+                        Frameworks : React.js, Express.js, MongoDB, Mongoose.js,
+                        Django.py
+                    </p>
+                </article>
+                <article id='contact-info'>
+                    <h3>Contact</h3>
+                    <p>Email : philaung96@gmail.com</p>
+                    <p id='social-icons'>
+                        <a
+                            href='https://www.linkedin.com/in/phil-aung-523b7b226/'
+                            target={'_blank'}
+                            rel='noreferrer'
+                        >
+                            <i class='fab fa-linkedin fa-2x'></i>
+                        </a>
+                        <a
+                            href='https://www.hackerrank.com/philaung96'
+                            target={'_blank'}
+                            rel='noreferrer'
+                        >
+                            <i class='fab fa-hackerrank fa-2x'></i>
+                        </a>
+                        <a
+                            href='https://github.com/philaung96'
+                            target={'_blank'}
+                            rel='noreferrer'
+                        >
+                            <i class='fab fa-github-square fa-2x'></i>
+                        </a>
+                    </p>
+                </article>
+            </aside>
+        </div>
     );
 };
 
