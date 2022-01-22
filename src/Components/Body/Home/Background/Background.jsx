f196826fd6eef65a28cd191501d6afe7aed53167
philaung96 <philaung96@gmail.com>
2022-01-21 20:25:24 -0800

    home page stars changed to only 3 and star positions adjusted based on screen size;

--- src/Components/Body/Home/Background/Background.jsx
+++ src/Components/Body/Home/Background/Background.jsx
@@ -1,17 +1,16 @@
 import './Background.css'
 
 const Background = () => {
-    const shootingStarsJsx = (num) => {
-        return [...Array(num)].map((item, i) => (
-            <span className='shooting_star' key={i}></span>
-        ));
-    };
     return (
         <div id='background'>
             <section id='space'>
                 <div className='star'></div>
             </section>
-            <section className='night'>{shootingStarsJsx(5)}</section>
+            <section className='night'>
+                <span className='shooting_star'></span>
+                <span className='shooting_star'></span>
+                <span className='shooting_star'></span>
+            </section>
         </div>
     );
 }
